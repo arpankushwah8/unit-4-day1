58e51b3917e7329be8e13d3f117acd5e4acf557e
arpankushwah8 <96377007+arpankushwah8@users.noreply.github.com>
2022-03-24 11:42:55 +0530

    a

---
+++ main.js
@@ -0,0 +1,16 @@
+const add = require ("./add.js");
+
+let a=56; let b=4;
+
+console.log(add(a,b));
+
+const divide= require ("./divide.js");
+
+console.log(divide(a,b));
+
+const multiply = require ("./multiply");
+
+console.log(multiply(a,b));
+
+const subtract = require ("./subtract");
+console.log(subtract(a,b));
